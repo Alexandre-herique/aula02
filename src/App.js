5dbe5415079f038e33df9ebab81180d7228988dc
Alexandre-herique <64115785+Alexandre-herique@users.noreply.github.com>
2020-12-03 19:35:49 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
+
+  // const [meuEstado, alteraEstado] = useState('outra informação');
+  const [usuario, alteraUsuario] = useState([]);
+  const [meuNome, alteraNome] = useState('');
+  const [meuEmail, alteraEmail] = useState('');
+  const [meuTel, alteraTel] = useState('');
+
+  function digitaNome (e){
+    alteraNome(e.target.value)
+  }
+  function digitaEmail (e){
+    alteraEmail(e.target.value)
+  }
+  
+  function digitaTel (e){
+    alteraTel(e.target.value)
+  }
+
+  let adicionar = e => {
+
+    let objetoUsuario = {
+      nome: meuNome,
+      email: meuEmail,
+      telefone: meuTel
+    };
+
+    usuario.push(objetoUsuario)
+
+    
+    popula()
+  }
+  
+  
+
+  let popula = () => {
+    var dados = ''
+
+    usuario.forEach(item =>{
+      dados +=  '<td>'+item.meuNome+'</td>'+
+                '<td>'+item.meuEmail+'</td>'+
+                '<td>'+item.meuTel+'</td>';
+              
+    });
+
+    document.getElementById('usuario').innerHTML= dados
+  }
+  // function editaDados (){
+    // alteraEstado('trocou os dados!')
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Digite os dados </p>
+      <input type="text" onChange={digitaNome} value= {meuNome} /> 
+      <input type="text" onChange={digitaEmail} value= {meuEmail} /> 
+      <input type="text" onChange={digitaTel} Value= {meuTel} /> 
+
+      <p>nome {meuNome} </p>
+      <p>email {meuEmail} </p>
+      <p>Tel {meuTel} </p>
+       <button onClick={adicionar}>send</button> 
+      <table>
+        <tr id="usuario" ></tr>
+      </table>
+
+      
     </div>
   );
 }
