79e228d1af89732702ac21ad468af8dd5e122cec
Alexandre-herique <64115785+Alexandre-herique@users.noreply.github.com>
2020-12-09 21:45:06 -0300

    aqula dia 9-12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
 
 
 function App() {
 
   // const [meuEstado, alteraEstado] = useState('outra informação');
-  const [usuario, alteraUsuario] = useState([]);
+  const [usuario,] = useState([]);
   const [meuNome, alteraNome] = useState('');
   const [meuEmail, alteraEmail] = useState('');
   const [meuTel, alteraTel] = useState('');
+  const [valorA, alteraA] = useState('');
+  const [valorB, alteraB] = useState('');
+  const [resultado, somaResultado] = useState('');
+ 
+  
+  
+
+  useEffect(function (){
+     alteraNome(meuNome.toUpperCase())
+     alteraEmail(meuEmail.toLowerCase())
+     somaResultado(resultado.toLowerCase())
+
+
+    //  alteraTel(meuTel.to)
+
+  },[meuNome, meuEmail, resultado])
 
   function digitaNome (e){
     alteraNome(e.target.value)
@@ -20,6 +36,9 @@ function App() {
     alteraTel(e.target.value)
   }
 
+  
+
+
   let adicionar = e => {
 
     let objetoUsuario = {
@@ -28,25 +47,70 @@ function App() {
       telefone: meuTel
     };
 
-    usuario.push(objetoUsuario)
+    usuario.push(objetoUsuario);
 
     
-    popula()
+    popula();
+  };
+
+
+  let adicionar2 = e => {
+
+    function somaValores (){
+      var s1 = parseInt(valorA)
+      var s2 = parseInt(valorB)
+      var s3 = (s1 + s2) 
+  
+      return(s3)
+    }
+
+    resultado.push(somaValores);
+
+    
+    popula2();
+  };
+
+  function digitaA (e){
+    alteraA(e.target.value)
   }
+
+  function digitaB (e){
+    alteraB(e.target.value)
+  }
+  
+
+
+
   
   
 
   let popula = () => {
-    var dados = ''
+    var dados = '';
 
     usuario.forEach(item =>{
-      dados +=  '<td>'+item.meuNome+'</td>'+
-                '<td>'+item.meuEmail+'</td>'+
-                '<td>'+item.meuTel+'</td>';
+      dados +=  '<tr>'+
+                  '<td>'+item.meuNome+'</td>'+
+                  '<td>'+item.meuEmail+'</td>'+
+                  '<td>'+item.meuTel+'</td>'+
+                '</tr>'
               
     });
 
-    document.getElementById('usuario').innerHTML= dados
+    document.getElementById('usuario').innerHTML= dados;
+
+  }
+
+
+  let popula2 = () => {
+    var dados2 = '';
+
+    resultado.forEach(item =>{
+      dados2 +=  <p> Seu resultado é: {resultado} </p>
+              
+    });
+
+    document.getElementById('resultado').innerHTML= dados2;
+
   }
   // function editaDados (){
     // alteraEstado('trocou os dados!')
@@ -54,21 +118,34 @@ function App() {
 
   return (
     <div>
-      <p>Digite os dados </p>
-      <input type="text" onChange={digitaNome} value= {meuNome} /> 
-      <input type="text" onChange={digitaEmail} value= {meuEmail} /> 
-      <input type="text" onChange={digitaTel} Value= {meuTel} /> 
+      <div>
 
-      <p>nome {meuNome} </p>
-      <p>email {meuEmail} </p>
-      <p>Tel {meuTel} </p>
-       <button onClick={adicionar}>send</button> 
-      <table>
-        <tr id="usuario" ></tr>
+        <p>Digite os dados </p>
+        <input type="text" onChange={digitaNome} value={meuNome} /> 
+        <input type="text" onChange={digitaEmail} value={meuEmail} /> 
+        <input type="text" onChange={digitaTel} Value={meuTel} /> 
+
+        <p>nome {meuNome} </p>
+        < p>email {meuEmail} </p>
+        <p>Tel {meuTel} </p>
+        <button onClick={adicionar}>send</button> 
+        <table id="usuario">
+        
       </table>
+      </div>
+      <div>
+       <p>conta</p>
+
+       Valor 1<input type="number" onChange={digitaA} value={valorA} /> 
+       Valor 2<input type="number" onChange={digitaB} value={valorB} /> 
+       <button onClick={adicionar2}>conta</button> 
+       <p id='resultado'></p>
+
+      </div>
 
       
     </div>
+    
   );
 }
 
